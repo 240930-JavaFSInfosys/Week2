2a26221519c246aae98651f62f882d9957917b57
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-10-10 11:10:21 -0400

    talked referential integrity and CASCADE

--- DML_Constraints.sql
+++ DML_Constraints.sql
@@ -100,18 +100,67 @@ SELECT now(); --returns the current date/time
 SELECT upper('this text will be returned all uppercase')
 
 --Aggregate functions can take in multiple values and return one value 
-SELECT avg(role_salary) FROM roles
+SELECT avg(role_salary) FROM roles --gets the average
+SELECT sum(role_salary) FROM roles --gets the sum
+SELECT count(employee_id) FROM employees --gives us a number of records that are returned
+SELECT count(employee_id) FROM employees WHERE role_id_fk = 1
 
+
+--GROUP BY will merge rows together based on matching column values
+SELECT count(*) FROM roles GROUP BY role_salary;
+
+--Maybe I want the role_salary as well, so the ResultSet is more clear
+SELECT role_salary, count(*) FROM roles GROUP BY role_salary;
+
+--HAVING is like a WHERE clause, but it can only be used after a GROUP BY. 
+--WHERE will not work after GROUP BY
+SELECT role_salary, count(*) FROM roles GROUP BY role_salary HAVING role_salary = 100000;
+
+--This is because the WHERE clause only works after a selection from tables (only works on raw table data)
+--"WHERE does NOT work on values that have been aggregated by GROUP BY" - nice QC line?
+
+
+--UPDATE--------------------------------------
 
 --We can use UPDATE to update records - let's say cashiers got a raise
 UPDATE roles SET role_salary = 45000 WHERE role_title = 'Cashier';
 
+--NOTE: the WHERE clause is very important here, otherwise we'd set every role to have the same salary
+
 SELECT * FROM roles; --NOTE: we could just run the previous select from above
+
+--DELETE (and a note about CASCADE)--------------------------------------
 
 --We can use DELETE to delete records - squidward is making too much money now - let's fire him
 DELETE FROM employees WHERE first_name = 'Squidward';
 
 SELECT * FROM employees;
+
+--Let's try to delete a Role (and see why it doesn't work)
+DELETE FROM roles WHERE role_title = 'Manager'
+--Can't do it! There are employee records that are pointing to the Manager role
+--"If you try to delete records or tables that are references by other records or tables, you can't"
+
+/* Why can't we delete?
+  
+   This is a built in rule, to promote REFERENTIAL INTEGRITY
+   "We can't have records that refer to other records that don't exist" - those are called orphan records 
+  
+  In order for this delete to work, we would have to say "ON DELETE CASCADE" in our foreign key column
+  observe below: (not actually going to run this table creation) */
+
+CREATE TABLE employees(
+	employee_id serial PRIMARY KEY, 
+	first_name TEXT NOT NULL, 
+	last_name TEXT NOT NULL,
+	role_id_fk int REFERENCES roles(role_id) ON DELETE CASCADE -- this also works for ON UPDATE CASCADE
+);
+
+--we could have also done ON DELETE SET NULL if we want to preserve the record but not FK
+
+
+
+
 
 
 
